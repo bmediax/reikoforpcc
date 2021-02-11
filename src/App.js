6ebb5dbbcd1c1f5e5f0e37d9692c6991b42e0760
bmediax <contact@bmediax.com>
2021-02-10 23:44:06 -0800

    Moved to /components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Header from './Header'
-import NotifyMe from './NotifyMe'
-import SocialMediaBtns from './SocialMediaBtns'
-import Footer from "./Footer"
+import Header from './components/Header'
+import NotifyMe from './components/NotifyMe'
+import SocialMediaBtns from './components/SocialMediaBtns'
+import Footer from "./components/Footer"
 
 const App = () => {
   return (

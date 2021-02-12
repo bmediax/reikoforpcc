f27b3e9ed22257544823423ba1670bc10707859b
bmediax <contact@bmediax.com>
2021-02-11 19:52:15 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React from 'react'
 import Header from './components/Header'
 import Contact from './components/Contact'
 import Footer from "./components/Footer"
 import Logo from './assets/logo.png'
+import { Helmet } from "react-helmet"
 
 const App = () => {
   return (
-    <div className="RMW_container">
-      <div className="RMW_wrapper">
-        <img src={Logo} alt="logo" id="logo" />
-        <Header />
-        <Contact />
-        <Footer />
+    <React.Fragment>
+      <Helmet>
+        <title> Reiko for PCC </title>
+        <meta name="description" content="Reiko Mia Williams for PCC Board Zone 7" />
+        <link rel="icon" type="image/png" href={Logo} sizes="16x16" />
+      </Helmet>
+      <div className="RMW_container">
+        <div className="RMW_wrapper">
+          <img src={Logo} alt="logo" id="logo" />
+          <Header />
+          <Contact />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 

481e5a3065782ecfc8475bb2dbe5b6230ca64fd5
bmediax <contact@bmediax.com>
2021-02-11 16:53:35 -0800

    - Remove Footer Fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,6 @@ const Contact = () => {
     return (
         <div id="contact">
             <span id="notify-me">
-                <span class="ex-icon">x</span>
                 <h2>Notify Me</h2>
                 <span class="arrow-icon"> > </span>
             </span>

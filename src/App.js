0191e509473ced70a792dbb799b434c4a815a92c
bmediax <contact@bmediax.com>
2021-02-12 22:15:25 -0800

    Navigation built

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react'
-import Header from './components/Header'
-import Contact from './components/Contact'
-import Footer from "./components/Footer"
 import Logo from './assets/logo.png'
+import Nav from './components/Nav'
 import { Helmet } from "react-helmet"
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <React.Fragment>
+    <Router>
       <Helmet>
-        <title> Reiko for PCC </title>
+        <title> Reiko 4 PCC </title>
         <meta name="description" content="Reiko Mia Williams for PCC Board Zone 7" />
         <link rel="icon" type="image/png" href={Logo} sizes="16x16" />
       </Helmet>
       <div className="RMW_container">
         <div className="RMW_wrapper">
-          <img src={Logo} alt="logo" id="logo" />
-          <Header />
-          <Contact />
-          <Footer />
+          <Nav logo={Logo} />
         </div>
       </div>
-    </React.Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+
+const Nav = (props) => {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <div className="logo-contain">
+                    <Link to="/" exact="true">
+                        <img src={props.logo} alt="Rieko Mia Williams For PCC Logo" id="logo" />
+                    </Link>
+                </div>
+                <div className="nav-contain">
+                    <Link className="nav-item" to="/meet-rieko">Meet Reiko</Link>
+                    <Link className="nav-item" to="/priorities">Priorities</Link>
+                    <Link className="nav-item" to="/endorsements">Endorsements</Link>
+                </div>
+                <div className="donate-contain">
+                    <a href="https://google.com" class="donate">Donate</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;

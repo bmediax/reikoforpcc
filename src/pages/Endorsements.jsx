5ba363fdd3cdcdea3fe197bc63f97ccf67cdc739
bmediax <contact@bmediax.com>
2021-02-12 23:36:50 -0800

    Helmet on all pages with correct title

--- src/pages/Endorsements.jsx
+++ src/pages/Endorsements.jsx
@@ -1,6 +1,14 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
 const Endorsements = () => {
     return (
-        <h1> Endorsements </h1>
+        <React.Fragment>
+            <Helmet>
+                <title> Rieko 4 PCC | Endorsements </title>
+            </Helmet>
+            <h1> Endorsements </h1>
+        </React.Fragment>
     )
 }
 
--- src/pages/Priorities.jsx
+++ src/pages/Priorities.jsx
@@ -1,6 +1,14 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
 const Priorities = () => {
     return (
-        <h1> Priorities </h1>
+        <React.Fragment>
+            <Helmet>
+                <title> Rieko 4 PCC | Priorities </title>
+            </Helmet>
+            <h1> Priorities </h1>
+        </React.Fragment>
     )
 }
 

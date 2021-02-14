580bd3704be6a2851b55f08b085ae795396b7f8c
bmediax <contact@bmediax.com>
2021-02-13 22:26:10 -0800

    Stay Tuned Component Styling and method

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,10 +4,10 @@ const Homepage = () => {
             <div id="slider">
                 <div className="stay-tuned">
                     <span className="tuned-title">Stay Tuned</span>
-                    <div className="tuned-item">
-                        <input className="tuned-input" type="text"  placeholder="Email Address" />
-                        <span className="tuned-arrow">></span>
-                    </div>
+                    <form className="tuned-item">
+                        <input className="tuned-input" type="email"  placeholder="Email Address" />
+                        <input type="submit" className="tuned-arrow" value=">" />
+                    </form>
                 </div>
             </div>
         </div>

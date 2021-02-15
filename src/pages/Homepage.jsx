1082370b6106e7f33299e5a6bd6bbe8af2996058
bmediax <contact@bmediax.com>
2021-02-14 17:08:12 -0800

    Importaing Problems

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,16 @@
 const Homepage = () => {
     return (
         <div className="container">
-            <div id="slider">
-                <div className="stay-tuned">
+            {/* <div id="slider"> */}
+                {/* Stayed Tuned */}
+                {/* <div className="stay-tuned">
                     <span className="tuned-title">Stay Tuned</span>
                     <form className="tuned-item">
                         <input className="tuned-input" type="email"  placeholder="Email Address" />
                         <input type="submit" className="tuned-arrow" value=">" />
                     </form>
-                </div>
-            </div>
+                </div> */}
+            {/* </div> */}
         </div>
     )
 }

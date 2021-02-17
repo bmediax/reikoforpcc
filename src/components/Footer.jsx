e5c6b4f32b94d9fcd6e39bcbd05db266644bdbe3
bmediax <contact@bmediax.com>
2021-02-16 18:43:30 -0800

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
+
 const Footer = (props) => {
     return (
         <footer>
             <div id="footer-wrapper">
-                <img src={props.logo} alt="Logo" width="80" />
+                <div className="footer-logo">
+                    <img src={props.logo} alt="Reiko for PCC Logo" width="60px"/>
+                </div>
+                <ul className="footer-nav">
+                    <li><Link to="/meet-reiko">Meet Reiko</Link></li>
+                    <li><Link to="/priorities">Priorities</Link></li>
+                    <li><Link to="/endorsements">Endorsements</Link></li>
+                </ul>
+                <div className="footer-social">
+                    <span>FB</span>
+                    <span>INSTA</span>
+                    <span>TWITTER</span>
+                </div>
             </div>
         </footer>
     )

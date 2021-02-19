add5f5b46c13960e11981729d3aed9c1fe9d1d9e
bmediax <contact@bmediax.com>
2021-02-19 14:13:16 -0800

    Meet Image

--- src/modules/ProfileModule.jsx
+++ src/modules/ProfileModule.jsx
@@ -3,7 +3,8 @@ import './ProfileModule.scss'
 const ProfileModule = (props) => {
     return (
         <div className="profile-module">
-            <span className="profile-module_img" style={{backgroundImage: `url(${props.img})`}}></span>
+            {/* <span className="profile-module_img" style={{backgroundImage: `url(${props.img})`}}>.</span> */}
+            <img className="profile-module_img" src={props.img} alt="Profile Headshot" />
             <span className="profile-module_textarea">
                 <p>{props.descr}</p>
                 <a href={props.link} className="btn minimal-btn">{props.linkText}</a>

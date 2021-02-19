2f58756e44bd6ac517319a49f32b0211c6e58dc0
bmediax <contact@bmediax.com>
2021-02-19 15:16:57 -0800

    Profile Module Finished

--- src/modules/ProfileModule.jsx
+++ src/modules/ProfileModule.jsx
@@ -1,13 +1,13 @@
+import { Link } from 'react-router-dom'
 import './ProfileModule.scss'
 
 const ProfileModule = (props) => {
     return (
         <div className="profile-module">
-            {/* <span className="profile-module_img" style={{backgroundImage: `url(${props.img})`}}>.</span> */}
             <img className="profile-module_img" src={props.img} alt="Profile Headshot" />
             <span className="profile-module_textarea">
                 <p>{props.descr}</p>
-                <a href={props.link} className="btn minimal-btn">{props.linkText}</a>
+                <Link to={props.link} className="btn minimal-btn">{props.linkText}</Link>
             </span>
         </div>
     )
--- src/pages/Homepage/Meetreiko.jsx
+++ src/pages/Homepage/Meetreiko.jsx
@@ -8,7 +8,7 @@ const Meetreiko = () => {
             <ProfileModule 
                 descr="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus sag   enenatis mi. Cras justo sem, volutpat tincidunt risus vitae, suscipit lobortis quam. Suspendisse sed odio erat. Nam odio lectus, tristique in lacus nec, condimentum malesuada nibh. Praesent quis metus risus. Etiam ut leo sit amet neque facilisis egestas. Sed sed ex quis orci ultricies." 
                 img="https://via.placeholder.com/300x250?text=Headshot%20Image" 
-                link="https://google.com" 
+                link="meet-reiko" 
                 linkText="Learn More" />
         </section>
     )

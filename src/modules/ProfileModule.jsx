5e7a1feae546976ef5ee8c16df36bf05e4046590
bmediax <contact@bmediax.com>
2021-02-19 13:47:31 -0800

    Dark Mode Meet Fix

--- src/modules/ProfileModule.jsx
+++ src/modules/ProfileModule.jsx
@@ -1,12 +1,12 @@
 import './ProfileModule.scss'
 
-const ProfileModule = () => {
+const ProfileModule = (props) => {
     return (
         <div className="profile-module">
-            <span className="profile-module_img" style={{"background": "url('https://via.placeholder.com/300x250?text=Headshot%20Image')"}}></span>
+            <span className="profile-module_img" style={{backgroundImage: `url(${props.img})`}}></span>
             <span className="profile-module_textarea">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus sag   enenatis mi. Cras justo sem, volutpat tincidunt risus vitae, suscipit lobortis quam. Suspendisse sed odio erat. Nam odio lectus, tristique in lacus nec, condimentum malesuada nibh. Praesent quis metus risus. Etiam ut leo sit amet neque facilisis egestas. Sed sed ex quis orci ultricies.</p>
-                <a href="https://google.com" className="btn minimal-btn">Learn More</a>
+                <p>{props.descr}</p>
+                <a href={props.link} className="btn minimal-btn">{props.linkText}</a>
             </span>
         </div>
     )
--- src/pages/Homepage/Meetreiko.jsx
+++ src/pages/Homepage/Meetreiko.jsx
@@ -5,7 +5,11 @@ const Meetreiko = () => {
         <section className="meet-reiko_section">
             <h1> Meet Reiko Mia Williams </h1>
 
-            <ProfileModule />
+            <ProfileModule 
+                descr="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus sag   enenatis mi. Cras justo sem, volutpat tincidunt risus vitae, suscipit lobortis quam. Suspendisse sed odio erat. Nam odio lectus, tristique in lacus nec, condimentum malesuada nibh. Praesent quis metus risus. Etiam ut leo sit amet neque facilisis egestas. Sed sed ex quis orci ultricies." 
+                img="https://via.placeholder.com/300x250?text=Headshot%20Image" 
+                link="https://google.com" 
+                linkText="Learn More" />
         </section>
     )
 }

dc6a539febd7c0bd04072827bfa0f31125ab2161
bmediax <contact@bmediax.com>
2021-02-19 22:32:30 -0800

    Title to Meet Reiko Section

--- src/modules/ProfileModule.jsx
+++ src/modules/ProfileModule.jsx
@@ -6,6 +6,7 @@ const ProfileModule = (props) => {
         <div className="profile-module">
             <img className="profile-module_img" src={props.img} alt="Profile Headshot" />
             <span className="profile-module_textarea">
+                <h3>{props.title}</h3>
                 <p>{props.descr}</p>
                 <Link to={props.link} className="btn minimal-btn" style={{marginTop:"15px"}}>{props.linkText}</Link>
             </span>
--- src/pages/Homepage/Meetreiko.jsx
+++ src/pages/Homepage/Meetreiko.jsx
@@ -7,6 +7,7 @@ const Meetreiko = () => {
             <h1> Meet Reiko Mia Williams </h1>
 
             <ProfileModule 
+                title="Who am I?" 
                 descr="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus sag   enenatis mi. Cras justo sem, volutpat tincidunt risus vitae, suscipit lobortis quam. Suspendisse sed odio erat. Nam odio lectus, tristique in lacus nec, condimentum malesuada nibh. Praesent quis metus risus. Etiam ut leo sit amet neque facilisis egestas. Sed sed ex quis orci ultricies." 
                 img="https://via.placeholder.com/300x250?text=Headshot%20Image" 
                 link="meet-reiko" 

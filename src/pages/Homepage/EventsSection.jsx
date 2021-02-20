889396abcb142a48c3110fabad3dfc1f5997a334
bmediax <contact@bmediax.com>
2021-02-19 22:18:22 -0800

    Events Page with Dumy Text

--- src/pages/Homepage/EventsSection.jsx
+++ src/pages/Homepage/EventsSection.jsx
@@ -13,11 +13,15 @@ const EventsSection = () => {
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>
                         <a href="https://google.com" className="btn blue-btn">View Event</a>
                     </span>
-                    <span className="event-img" style={{background: "url('https://via.placeholder.com/420x250?text=Events%20Image')"}}></span>
+                    <span className="event-img" style={{background: "url('https://via.placeholder.com/420x250?text=Events%20Image')"}} />
                 </span>
                 <span className="events-module_section">
-                    <span className="event-img" style={{background: "url('https://via.placeholder.com/420x250?text=Events%20Image')"}}></span>
-                    <span className="event-des"></span>
+                    <span className="event-img" style={{background: "url('https://via.placeholder.com/420x250?text=Events%20Image')"}} />
+                    <span className="event-des">
+                    <h3>Campaign Kick - Off</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>
+                        <a href="https://google.com" className="btn blue-btn">View Event</a>
+                    </span>
                 </span>
             </div>
         </SectionTemplate>

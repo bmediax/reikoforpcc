cc0b430144547852becb55ea349de459d5fd8b86
Bmediax <contact@bmediax.com>
2021-02-20 19:08:22 -0800

    Failed AXIOS

--- src/axios.js
+++ src/axios.js
@@ -1,3 +1,3 @@
-import Axios from 'axios'
+// import Axios from 'axios'
 
-export const axios = Axios.create({ baseURL: "https://calndr.link/api/events/9KSRrUdaTG" })
+// export const axios = Axios.create({ baseURL: "https://calndr.link/api/events/9KSRrUdaTG" })
--- src/pages/Homepage/EventsSection.jsx
+++ src/pages/Homepage/EventsSection.jsx
@@ -1,21 +1,20 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import SectionTemplate from '../../components/SectionTemplate'
 import { RiCalendarEventLine } from "react-icons/ri";
-import { axios } from '../../axios'
 
 const EventsSection = () => {
-
-    const [caldata, setCaldata] = useState("");
-
-    const getCaldata = async () => {
-        const response = await axios.get("/").catch( (err) => console.log("Error: ", err))
-
-        if (response && response.data) setCaldata(response.data)
-    }
+    // Set State on Calendar Data API
+    const [caldata, setCaldata] = useState();
 
     useEffect(() => {
-        getCaldata()
-    }, [])
+        const fetchCaldata = async () => {
+            const response = await axios("https://calndr.link/api/events/9KSRrUdaTG");
+            setCaldata(response.data);
+        }
+        fetchCaldata();
+    }, []);
+
     return (
         <SectionTemplate topic="events" tag="events">
             <h1> Events </h1>
@@ -27,7 +26,9 @@ const EventsSection = () => {
                         <h3>Campaign Kick - Off</h3>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>
                         <a href="https://google.com" className="btn blue-btn">View Event <RiCalendarEventLine  className="btn-icon" /></a>
-                         <a href={caldata.links}>Cal API Link</a>
+                        {caldata.map(calendars => (
+                            <button> {calendars.links} </button>
+                        ))}
                     </span>
                     <span className="event-img" style={{background: "url('https://via.placeholder.com/420x250?text=Events%20Image')"}} />
                 </span>

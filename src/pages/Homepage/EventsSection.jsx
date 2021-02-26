16b9211d7f3cfd2dfa80fff6b8001c7a403af6f0
bmediax <contact@bmediax.com>
2021-02-26 15:47:57 -0800

    Event Past class, updated icons

--- src/pages/Homepage/EventsSection.jsx
+++ src/pages/Homepage/EventsSection.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import SectionTemplate from '../../components/SectionTemplate'
-import { RiCalendarEventLine } from "react-icons/ri";
+import { BiLinkExternal } from "react-icons/bi";
+import { IoLogoGoogle } from 'react-icons/io'
+import { SiMicrosoftoutlook } from 'react-icons/si'
 import axios from '../../tools/axios'
 
 const EventsSection = () => {
@@ -33,13 +35,13 @@ const EventsSection = () => {
                         <p>{caldata.data.description}</p>
                         <div className="event-links">
                             <a href={caldata.links.event_page} className="small-btn blue-btn">
-                                View Event Page<RiCalendarEventLine  className="btn-icon" />
+                                View Event Page<BiLinkExternal  className="btn-icon" />
                             </a>
                             <a href={caldata.links.google} className="small-btn blue-btn">
-                                Add to Google <RiCalendarEventLine  className="btn-icon" />
+                                Add to Google <IoLogoGoogle  className="btn-icon" />
                             </a>
                             <a href={caldata.links.office365} className="small-btn blue-btn">
-                                Add to Outlook<RiCalendarEventLine  className="btn-icon" />
+                                Add to Outlook<SiMicrosoftoutlook  className="btn-icon" />
                             </a>
                         </div>
                     </span>
@@ -48,27 +50,6 @@ const EventsSection = () => {
                         <span className="event-date_day">23</span>
                     </span>
                 </span>
-                {/* <span className="events-module_section">
-                    <span className="event-date">
-                        <span className="event-date_month">February</span>
-                        <span className="event-date_day">23</span>
-                    </span>
-                    <span className="event-des">
-                        <h3>{caldata.data.title}</h3>
-                        <p>{caldata.data.description}</p>
-                        <div className="event-links">
-                            <a href={caldata.links.event_page} className="small-btn blue-btn">
-                                View Event Page<RiCalendarEventLine  className="btn-icon" />
-                            </a>
-                            <a href={caldata.links.google} className="small-btn blue-btn">
-                                Add to Google <RiCalendarEventLine  className="btn-icon" />
-                            </a>
-                            <a href={caldata.links.office365} className="small-btn blue-btn">
-                                Add to Outlook<RiCalendarEventLine  className="btn-icon" />
-                            </a>
-                        </div>
-                    </span>
-                </span> */}
                 {/* <span className="event-des">
                     <h3>Campaign Kick - Off</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>

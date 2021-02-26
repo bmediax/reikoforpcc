eaf59cab3758ad5cf31d571c39fa3ae4c56938fa
bmediax <contact@bmediax.com>
2021-02-26 14:32:15 -0800

    Add Event Buttons and API

--- src/pages/Homepage/EventsSection.jsx
+++ src/pages/Homepage/EventsSection.jsx
@@ -1,41 +1,24 @@
 import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
 import SectionTemplate from '../../components/SectionTemplate'
-import { RiCalendarEventLine, RiContrastDropLine } from "react-icons/ri";
-// import { eventsData } from '../../data/eventsData'
+import { RiCalendarEventLine } from "react-icons/ri";
+import axios from '../../tools/axios'
 
 const EventsSection = () => {
-    const [caldata, setCaldata] = useState("");
+    const [caldata, setCaldata] = useState('');
+    const [loader, setLoader] = useState(false)
 
-    const getCaldata = () => {
-        Axios.get('https://calndr.link/api/events/9KSRrUdaTG').then((response) => {
-            const calApiData = response.data
-            setCaldata(calApiData)
-        }).catch(error => console.error(error))
-    }
-
-    useEffect( () => {
-        getCaldata();
+    useEffect(() => {
+        async function fetchCalData() {
+            const response = await axios.get("/9KSRrUdaTG")
+            setCaldata(response.data)
+            setLoader(true)
+            return response
+        }
+        fetchCalData()
     }, [])
 
-    const displayCaldata = () => {
-        if (caldata != null ) {
-            return (
-                <span className="events-module_section">
-                    <span className="event-des">
-                        <h3>{caldata.data.title}</h3>
-                        <p>{caldata.data.description}</p>
-                        <a target="_blank" rel="noreferrer" href={caldata.data.link} className="btn blue-btn">View Event <RiCalendarEventLine  className="btn-icon" /></a>
-                    </span>
-                    <span className="event-date">
-                        <span className="event-date_month">February</span>
-                        <span className="event-date_day">23</span>
-                    </span>
-                </span>
-            )
-        } else {
-            return (<h1>No Calendar Data Yet</h1>)
-        }
+    if (!loader) {
+        return <div align="center">Loading</div>
     }
 
     return (
@@ -44,7 +27,28 @@ const EventsSection = () => {
 
             {/* Events Module */}
             <div className="events-module">
-                {displayCaldata()}
+                <span className="events-module_section">
+                    <span className="event-des">
+                        <h3>{caldata.data.title}</h3>
+                        <p>{caldata.data.description}</p>
+
+                        <div className="event-links">
+                            <a target="_blank" rel="noreferrer" href={caldata.data.link} className="small-btn blue-btn">
+                                View Event Page<RiCalendarEventLine  className="btn-icon" />
+                            </a>
+                            <a target="_blank" rel="noreferrer" href={caldata.data.link} className="small-btn blue-btn">
+                                Add to Google <RiCalendarEventLine  className="btn-icon" />
+                            </a>
+                            <a target="_blank" rel="noreferrer" href={caldata.data.link} className="small-btn blue-btn">
+                                Add to Outlook<RiCalendarEventLine  className="btn-icon" />
+                            </a>
+                        </div>
+                    </span>
+                    <span className="event-date">
+                        <span className="event-date_month">February</span>
+                        <span className="event-date_day">23</span>
+                    </span>
+                </span>
                 {/* <span className="event-des">
                     <h3>Campaign Kick - Off</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>

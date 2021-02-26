f0d020f8f7211aa9af40a06a53535fcb13b1d578
Bmediax <contact@bmediax.com>
2021-02-26 12:56:39 -0800

    events

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,11 +1,8 @@
-import RmwGroup from '../assets/imgs/RMW-group.jpg'
-
 export const eventsData = [
     {
         title: "Campaign Kick - Off",
         description: "Join me for my first campaign event! Come learn about my priorities and why representation matters. Speakers at the event include: Alex Díaz Rios, Zone 7 PCC Board Director Olivia Alcaire, Hillsboro City Councilor Citlalli Nuñez, PCC Student And More!",
         link: "https://calndr.link/event/9KSRrUdaTG",
-        img: RmwGroup,
         id: 1
     }
 ]
--- src/pages/Homepage/EventsSection.jsx
+++ src/pages/Homepage/EventsSection.jsx
@@ -1,35 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 import SectionTemplate from '../../components/SectionTemplate'
-import { RiCalendarEventLine } from "react-icons/ri";
-import { eventsData } from '../../data/eventsData'
+import { RiCalendarEventLine, RiContrastDropLine } from "react-icons/ri";
+// import { eventsData } from '../../data/eventsData'
 
 const EventsSection = () => {
+    const [caldata, setCaldata] = useState("");
+
+    const getCaldata = () => {
+        Axios.get('https://calndr.link/api/events/9KSRrUdaTG').then((response) => {
+            const calApiData = response.data
+            setCaldata(calApiData)
+        }).catch(error => console.error(error))
+    }
+
+    useEffect( () => {
+        getCaldata();
+    }, [])
+
+    const displayCaldata = () => {
+        if (caldata != null ) {
+            return (
+                <span className="events-module_section">
+                    <span className="event-des">
+                        <h3>{caldata.data.title}</h3>
+                        <p>{caldata.data.description}</p>
+                        <a target="_blank" rel="noreferrer" href={caldata.data.link} className="btn blue-btn">View Event <RiCalendarEventLine  className="btn-icon" /></a>
+                    </span>
+                    <span className="event-date">
+                        <span className="event-date_month">February</span>
+                        <span className="event-date_day">23</span>
+                    </span>
+                </span>
+            )
+        } else {
+            return (<h1>No Calendar Data Yet</h1>)
+        }
+    }
+
     return (
         <SectionTemplate topic="events" tag="events">
             <h1> Events </h1>
 
             {/* Events Module */}
             <div className="events-module">
-                {eventsData.map(event => (
-                    <span key={event.id} className="events-module_section">
-                        <span className="event-des">
-                            <h3>{event.title}</h3>
-                            <p>{event.description}</p>
-                            <a target="_blank" rel="noreferrer" href={event.link} className="btn blue-btn">View Event <RiCalendarEventLine  className="btn-icon" /></a>
-                        </span>
-                        <span className="event-date">
-                            <span className="event-date_month">February</span>
-                            <span className="event-date_day">23</span>
-                        </span>
-                    </span>
-                ))}
-                {/* <span className="events-module_section">
-                    <span className="event-img" style={{background: "url('https://via.placeholder.com/420x250?text=Events%20Image')"}} />
-                    <span className="event-des">
+                {displayCaldata()}
+                {/* <span className="event-des">
                     <h3>Campaign Kick - Off</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>
-                        <a href="https://google.com" className="btn blue-btn">View Event <RiCalendarEventLine  className="btn-icon" /></a>
-                    </span>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>
+                    <a href="https://google.com" className="btn blue-btn">View Event <RiCalendarEventLine  className="btn-icon" /></a>
                 </span> */}
             </div>
         </SectionTemplate>

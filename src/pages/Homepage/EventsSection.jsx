0884c49b97d4060adaf1ca2f82e5de90e46c5f12
bmediax <contact@bmediax.com>
2021-02-26 15:28:20 -0800

    Fix every 2th event

--- src/pages/Homepage/EventsSection.jsx
+++ src/pages/Homepage/EventsSection.jsx
@@ -48,6 +48,27 @@ const EventsSection = () => {
                         <span className="event-date_day">23</span>
                     </span>
                 </span>
+                {/* <span className="events-module_section">
+                    <span className="event-date">
+                        <span className="event-date_month">February</span>
+                        <span className="event-date_day">23</span>
+                    </span>
+                    <span className="event-des">
+                        <h3>{caldata.data.title}</h3>
+                        <p>{caldata.data.description}</p>
+                        <div className="event-links">
+                            <a href={caldata.links.event_page} className="small-btn blue-btn">
+                                View Event Page<RiCalendarEventLine  className="btn-icon" />
+                            </a>
+                            <a href={caldata.links.google} className="small-btn blue-btn">
+                                Add to Google <RiCalendarEventLine  className="btn-icon" />
+                            </a>
+                            <a href={caldata.links.office365} className="small-btn blue-btn">
+                                Add to Outlook<RiCalendarEventLine  className="btn-icon" />
+                            </a>
+                        </div>
+                    </span>
+                </span> */}
                 {/* <span className="event-des">
                     <h3>Campaign Kick - Off</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi cumque aut fuga enim. Eius quam beatae velit, ad autem laudantium debitis harum natus temporibus rem obcaecati? Iste repudiandae, laborum nobis eaque illo odio, ab soluta fuga quod sequi suscipit exercitationem nam error quidem dicta quisquam blanditiis velit illum distinctio! Mollitia.</p>

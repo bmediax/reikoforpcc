8fb4b7190a32a3407dd426bcceb6378619f4e598
bmediax <contact@bmediax.com>
2021-03-01 14:15:43 -0800

    Added Form, Styling...

--- src/modules/GetinvolvedModule.jsx
+++ src/modules/GetinvolvedModule.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react'
 import SectionTemplate from '../components/SectionTemplate'
 import './GetinvolvedModule.scss'
 import PopupModule from './PopupModule'
 
 const GetinvolvedModule = () => {
+    const [ modal, setModal ] = useState(false)
+
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
     return (
         <SectionTemplate topic="get-involved" tag="getinvolved">
             <h1>Get involved</h1>
-            <button className="btn transparent-btn-solid"> I want to help out! </button>
-            <PopupModule>
-                <h3> Popup Module</h3>
+            <button onClick={toggleModal} className="btn transparent-btn-solid"> { modal ? "Hide" : "I want to help out!"}</button>
+            <PopupModule show={modal}>
+                <h3> Get Involved</h3>
+
+                <form className="getinvolved-form">
+                    <label for="first">First Name</label>
+                    <input type="text" id="first" placeholder="John" />
+
+                    <label for="last">Last Name</label>
+                    <input type="text" id="last" placeholder="Appleseed" />
+
+                    <label for="email">Email Address</label>
+                    <input type="email" id="email" placeholder="email@mail.com" />
+
+                    <label for="titleorg">Title / Organization</label>
+                    <input type="text" id="titleorg" placeholder="Title / Organization" />
+
+                    <div className="getinvolved-checkbox">
+                        <label>I'd like to</label><br />
+                        <input type="checkbox" id="phonebank" name="phonebank" value="phonebank" />
+                        <label htmlFor="phonebank">Phone Bank</label>
+                        <br />
+                        <input type="checkbox" id="lawnsign" name="lawnsign" value="lawnsign" />
+                        <label htmlFor="lawnsign">Receive Lawn Sign </label>
+                        <br />
+                        <input type="checkbox" id="endorse" name="endorse" value="endorse" />
+                        <label htmlFor="endorse">Endorse Reiko and I authorize her campaign to display my name publicly</label>
+                    </div>
+
+                    <input type="submit" value="send" className="btn minimal-btn" />
+                </form>
             </PopupModule>
         </SectionTemplate>
     )
--- src/modules/PopupModule.jsx
+++ src/modules/PopupModule.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './PopupModule.scss';
 
 const PopupModule = (props) => {
+    if (!props.show) {
+        return null
+    }
+
     return (
-        <>
-            <div className="modal-overlay" id="modal-overlay" />
-            <div className="modal" id="modal">
-                <button className="close-button" id="close-button">Obvious Close Button</button>
-                <div className="modal-guts">
-                    {props.children}    
-                </div>
-            </div>
-        </>
+        <div className="popup-modal_wrapper">
+            {props.children}
+        </div>
     );
 };
 

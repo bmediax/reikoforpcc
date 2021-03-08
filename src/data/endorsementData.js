0b3d91037c04c2ed0631f6e2e37ddc995c20b089
bmediax <contact@bmediax.com>
2021-03-07 19:31:21 -0800

    Updated Endorsements

--- src/data/endorsementData.js
+++ src/data/endorsementData.js
@@ -1,92 +1,140 @@
 export const endorsementData = [
     {
+        name: "Washington County Democrats",
+        position: "",
+        company:"",
+        id: 1
+    },
+    {
+        name: "Oregon Future Labs / Color Pac",
+        position: "",
+        company:"",
+        id: 2
+    },
+    {
+        name: "Chuck Riley",
+        position: "State Senator",
+        company:"",
+        id: 3
+    },
+    {
+        name: "Lew Frederick",
+        position: "State Presentative",
+        company:"",
+        id: 4
+    },
+    {
         name: "Janelle Bynum",
         position: "State Representative",
         company:"",
-        id: 1
+        id: 5
     },
     {
         name: "Joe Gallegos",
         position: "Former State Representative",
         company:"",
-        id: 2
+        id: 6
     },
     {
-        name: "Alexander Díaz Rios",
+        name: "Mohamad Ayjouri",
+        position: "PCC Board Chair",
+        company:"",
+        id: 7
+    },
+    {
+        name: "Tiffani Penson",
+        position:"PCC Board Vice-Chair",
+        company:"",
+        id: 8
+    },
+    {
+        name: "Alexnader Díaz Rios",
         position: "PCC Board Director Zone 7",
         company:"",
-        id: 3
+        id: 9
+    },
+    {
+        name: "Dan Saltzman",
+        position: "PCC Board Director Zone 5",
+        company:"",
+        id:10
+    },
+    {
+        name: "Kali Thorne Ladd",
+        position: "Former PCC Board Chair",
+        company:"",
+        id:11
+    },
+    {
+        name: "Nafisa Fai",
+        position: "Washington County Commisioner",
+        company:"",
+        id:12
     },
     {
         name: "Carmen Rubio",
         position: "Portland Commissioner",
         company:"",
-        id: 4
+        id:13
     },
     {
         name: "Olivia Alcaire",
         position: "City of Hillsboro Councilor",
         company:"",
-        id: 5
-    },
-    {
-        name: "Narce Rodriguez",
-        position: "Forest Grove School Board Director",
-        company:"",
-        id: 6
-    },
-    {
-        name: "Travis Nelson",
-        position: "",
-        company:"",
-        id: 7
-    },
-    {
-        name: "Danielle Garza",
-        position:"",
-        company:"",
-        id: 8
-    },
-    {
-        name: "Travis Nelson",
-        position: "Community Member",
-        company:"",
-        id: 9
-    },
-    {
-        name: "Danielle Garza",
-        position: "Community Member",
-        company:"",
-        id:10
-    },
-    {
-        name: "Jaime Rodriguez",
-        position: "Community Member",
-        company:"",
-        id:11
-    },
-    {
-        name: "Chuck Riley",
-        position: "State senator",
-        company:"",
-        id:12
-    },
-    {
-        name: "Dr. Katie Riley",
-        position: "Washington County Kids President",
-        company:"",
-        id:13
-    },
-    {
-        name: "Nafisa Fai",
-        position: "Washington County Commissioner",
-        company:"",
         id:14
     },
     {
-        name: "Dr. Olga Acuna",
+        name: "Dr. Olga Acuña",
         position: "Former Hillsboro City Councilor",
         company:"",
         id:15
+    },
+    {
+        name: "Narce Rodriguez",
+        position: "Former Forest Grove School Board Director",
+        company:"",
+        id:16
+    },
+    {
+        name: "Ben Bowmen",
+        position: "Tigard Tualatin School Board Director",
+        company:"",
+        id:17
+    },
+    {
+        name: "Travis Nelson",
+        position: "Democratic National Committee Member*",
+        company:"",
+        id:18
+    },
+    {
+        name: "Dr. Katie Riley",
+        position: "Washington County Kids President*",
+        company:"",
+        id:19
+    },
+    {
+        name: "Jaime Rodriguez",
+        position: "American Federation of Teachers - Oregon President*",
+        company:"",
+        id:20
+    },
+    {
+        name: "Danielle Garza",
+        position: "LiUNA Local 737 Political and Legislative Assistant*",
+        company:"",
+        id:21
+    },
+    {
+        name: "Citlalli Nuñez",
+        position: "Portland Community College Student",
+        company:"",
+        id:22
+    },
+    {
+        name: "Brian Moreno",
+        position: "Portland Community College Student",
+        company:"",
+        id:23
     }
 ]

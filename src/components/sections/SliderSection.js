4f55586d4f90b5bf0304c7377e77a721c45f3001
bmediax <contact@bmediax.com>
2021-03-10 18:52:27 -0800

    Slider before color changed

--- src/components/sections/SliderSection.js
+++ src/components/sections/SliderSection.js
@@ -5,7 +5,7 @@ const SliderSection = () => {
     return (
         <div id="slider">
             <span className="slider_text">
-                <StaticImage src="../../images/gfx/logo-icon_white.png" width={600} alt="Reiko for PCC" />
+                <StaticImage src="../../images/gfx/logo-icon_white.png" width={450} alt="Reiko for PCC" />
                 {/* <h1>Reiko Mia Williams</h1>
                 <p> for PCC Zone 7 </p> */}
             </span>

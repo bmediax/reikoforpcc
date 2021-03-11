4413baee7d701d3954d3fcee3697f27564c2f2a5
bmediax <contact@bmediax.com>
2021-03-10 18:48:24 -0800

    Added Logo to Cover

--- src/components/sections/SliderSection.js
+++ src/components/sections/SliderSection.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const SliderSection = () => {
     return (
         <div id="slider">
             <span className="slider_text">
-                <h1>Reiko Mia Williams</h1>
-                <p> for PCC Zone 7 </p>
+                <StaticImage src="../../images/gfx/logo-icon_white.png" width={600} alt="Reiko for PCC" />
+                {/* <h1>Reiko Mia Williams</h1>
+                <p> for PCC Zone 7 </p> */}
             </span>
         </div>
     )

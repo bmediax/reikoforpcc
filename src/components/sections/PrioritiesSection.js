c0949eab44c4ef8f3eb89f60fb31efbaea9f577c
Bmediax <contact@bmediax.com>
2021-03-16 10:03:36 -0700

    Priorities last object description commented out for consistency in sizing

--- src/components/sections/PrioritiesSection.js
+++ src/components/sections/PrioritiesSection.js
@@ -21,7 +21,7 @@ const PrioritiesSection = () => {
                     <div>
                         {/* <img src="https://via.placeholder.com/150" alt=""/> */}
                         <h2>Transparent commitment within the PCC Board</h2>
-                        <p>To nurture a collaborative culture of Diversity, Equity, and Inclusion among faculty, staff, and administrators throughout Portland Community College and beyond. </p>
+                        {/* <p>To nurture a collaborative culture of Diversity, Equity, and Inclusion among faculty, staff, and administrators throughout Portland Community College and beyond. </p> */}
                     </div>
                 </div>
             </div>

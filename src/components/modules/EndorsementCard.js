aafb78a460c050213539b5ae4f81387d245623d9
bmediax <contact@bmediax.com>
2021-04-05 22:48:31 -0700

    Fixed the endorsement view

--- src/components/modules/EndorsementCard.js
+++ src/components/modules/EndorsementCard.js
@@ -4,18 +4,39 @@ import { motion } from 'framer-motion'
 // import { cardVariants } from '../tools/motionVariants'
 
 const EndorsementCard = (props) => {
-    return (
-        <motion.div whileHover={{ scale: 1.05 }} className="endorsement-card">
-            {
-                props.logo ?
+    const isUrl = (isLink) => {
+        if (isLink) {
+            if (props.logo) {
+                return (
                     <a href={props.url}>
                         {props.logo}
                     </a>
-                :
-                <a href={props.url}>
-                    <h2> {props.name} </h2>
-                </a>
+                )
+            } 
+            
+            else {
+                return (
+                    <a href={props.url}>
+                        <h2> {props.name} </h2>
+                    </a>
+                )
             }
+        } else {
+            if (props.logo) {
+                return (props.logo)
+            } 
+            
+            else {
+                return (
+                    <h2> {props.name} </h2>
+                )
+            }
+        }
+    }
+    return (
+        <motion.div whileHover={{ scale: 1.05 }} className="endorsement-card">
+            {/* If url and if logo */}
+            {isUrl(props.url)}
             <p> {props.position}{props.company ? ", " + props.company : null}</p>
         </motion.div>
     )

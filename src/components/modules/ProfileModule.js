0290cdd4d8498252d20b765643ca8ff375c5a368
bmediax <contact@bmediax.com>
2021-04-06 16:13:21 -0700

    Animated and Changed Profile Image

--- src/components/modules/ProfileModule.js
+++ src/components/modules/ProfileModule.js
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { Link } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
 
@@ -9,13 +10,29 @@ import './ProfileModule.scss'
 // import RmwHeadshot from '../../images/photos/RMW-headshot.jpeg'
 
 const ProfileModule = (props) => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
+
     return (
         <div className="profile-module">
-            <span className="profile-module_img">
-                <StaticImage src="../../images/photos/RMW-profile.png" 
+            <motion.span className="profile-module_img" ref={ref}
+                animate={controls}
+                initial="hidden"
+                transition={{ duration: 1.5 , type: 'spring', damping: 10, stiffness: 100 }}
+                variants={{
+                    visible: { opacity: 1, y: 0 },
+                    hidden: { opacity: 0, y: 100 }
+                }}>
+                <StaticImage src="../../images/gfx/reiko-logo_mask.png" 
                     alt="Reiko Mia Williams Headshot" 
                     placeholder="blurred" height={400} />
-            </span>
+            </motion.span>
             <span className="profile-module_textarea">
                 <h3>{props.title}</h3>
                 <p>“It Takes A Village” reflects my life experiences. A black teenage parent, my Mother, took adult education programs (supported by an extended family) to earn her license as a practical nurse. While completing her B.A. in Nursing, she died of leukemia just before my 13th birthday.</p>

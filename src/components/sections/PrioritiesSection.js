dba82f64d4333c026bdc91a4a11323f0c5ef236c
bmediax <contact@bmediax.com>
2021-04-06 16:51:39 -0700

    :sparkles: Added icons

--- src/components/sections/PrioritiesSection.js
+++ src/components/sections/PrioritiesSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import SectionLayout from '../../layout/SectionLayout'
 
 const PrioritiesSection = () => {
@@ -9,17 +10,23 @@ const PrioritiesSection = () => {
             <div className="three-column-option_container">
                 <div className="three-column-option">
                     <div>
-                        {/* <img src="https://via.placeholder.com/150" alt=""/> */}
+                        <center>
+                            <StaticImage src="../../images/gfx/icons/circlePeople.png" alt="This is an imgae" width={120} />
+                        </center>
                         <h2> Inclusive, equitable education for all students.</h2>
                         {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, sequi</p> */}
                     </div>
                     <div>
-                        {/* <img src="https://via.placeholder.com/150" alt=""/> */}
+                        <center>
+                            <StaticImage src="../../images/gfx/icons/heartHand.png" alt="This is an image" width={120} />
+                        </center>
                         <h2>Stronger partnerships with K-12 school districts.</h2>
                         {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, sequi</p> */}
                     </div>
                     <div>
-                        {/* <img src="https://via.placeholder.com/150" alt=""/> */}
+                        <center>
+                            <StaticImage src="../../images/gfx/icons/eye.png" alt="This is an imgae" width={120} />
+                        </center>
                         <h2>Transparent commitment within the PCC Board</h2>
                         {/* <p>To nurture a collaborative culture of Diversity, Equity, and Inclusion among faculty, staff, and administrators throughout Portland Community College and beyond. </p> */}
                     </div>

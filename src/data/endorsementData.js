ca7d147c52019d726e518e41600d7778cfbe56bd
bmediax <contact@bmediax.com>
2021-04-05 21:58:59 -0700

    :sparkles: Added more endorsements!

--- src/data/endorsementData.js
+++ src/data/endorsementData.js
@@ -8,138 +8,180 @@ export const endorsementData = [
         logo: <StaticImage src="../images/endorsements/dwc-logo.png" placeholder="blurred" height={70} alt="Washington County Democrats Logo" />,
         position: "",
         company:"",
-        key: 1
+    },
+    {
+        name: "APANO",
+        url: 'https://www.apano.org/',
+        logo: "",
+        position: "",
+        company:"",
+    },
+    {
+        name: "Northwest Oregon Labor Council",
+        url: 'https://or.aflcio.org/nolc',
+        logo: "",
+        position: "",
+        company:"",
+    },
+    {
+        name: "PCC Federation of Classified Employees",
+        url: 'https://sites.google.com/site/pccfcelocal3922/home',
+        logo: "",
+        position: "",
+        company:"",
+    },
+    {
+        name: "UFCW Local 555",
+        url: 'https://www.ufcw555.org/',
+        logo: "",
+        position: "",
+        company:"",
+    },
+    {
+        name: "LiUNA Local 737",
+        url: 'https://www.local737.org/',
+        logo: "",
+        position: "",
+        company:"",
     },
     {
         name: "Oregon Futures Lab/Color PAC",
         url: "https://www.oregonfutureslab.org/",
         logo: <StaticImage src="../images/endorsements/ofl-logo.png" placeholder="blurred" height={70} alt="Oregon Futures Lab Logo" />,
-        key: 2
     },
     {
         name: "Chuck Riley",
         position: "State Senator",
         url: 'https://www.oregonlegislature.gov/riley',
-        key: 3
     },
     {
         name: "Lew Frederick",
         position: "State Senator",
         url: "https://www.oregonlegislature.gov/frederick",
-        key: 4
+    },
+    {
+        name: "Lew FrederickWlnsvey Campos",
+        position: "State Representative",
+        url: "",
     },
     {
         name: "Janelle Bynum",
         position: "State Representative",
         url: "https://www.oregonlegislature.gov/bynum",
-        key: 5
     },
     {
         name: "Joe Gallegos",
         position: "Former State Representative",
         company:"",
-        key: 6
     },
     {
         name: "Tiffani Penson",
         position:"PCC Board Vice-Chair",
         company:"",
-        key: 7
     },
     {
         name: "Alexander Díaz Rios",
         position: "PCC Board Director Zone 7",
         company:"",
-        key: 8
     },
     {
         name: "Dan Saltzman",
         position: "PCC Board Director Zone 5",
         company:"",
-        key:9
     },
     {
         name: "Kali Thorne Ladd",
         position: "Former PCC Board Chair",
         company:"",
-        key:10
     },
     {
         name: "Nafisa Fai",
         position: "Washington County Commissioner",
         url: "https://www.nafisaforwashingtoncounty.com/",
-        key:11
     },
     {
         name: "Lacey Beaty",
         position: "Beaverton Mayor",
         url: "https://beatyforbeaverton.com/",
-        key:12
     },
     {
         name: "Nadia Hasan",
         position: "Beaverton Councilor",
         url: "https://www.nadiahasan.com/",
-        key:13
     },
     {
         name: "Carmen Rubio",
         position: "Portland Commissioner",
         url: "https://www.carmenforportland.com/",
-        key:14
     },
     {
         name: "Olivia Alcaire",
         position: "City of Hillsboro Councilor",
         url: "https://oliviaforhillsboro.com/",
-        key:15
     },
     {
         name: "Dr. Olga Acuña",
         position: "Former City of Hillsboro Councilor",
         company:"",
-        key:16
     },
     {
         name: "Angeles Godinez",
         position: "City of Cornelius Councilor",
         url: "https://www.ci.cornelius.or.us/citycouncil/page/councilor-angeles-godinez",
-        key:17
     },
     {
         name: "Doris Gonzalez",
         position: "City of Cornelius Councilor",
         url: "https://www.ci.cornelius.or.us/citycouncil/page/councilor-doris-gonzalez",
-        key:18
+    },
+    {
+        name: "Scott Bailey",
+        position: "Portland Public Schools Board Vice Chair",
+        url: "",
     },
     {
         name: "Narce Rodriguez",
         position: "Forest Grove School Board Director",
         company:"",
-        key:19
     },
     {
         name: "Ben Bowman",
         position: "Tigard Tualatin School Board Director",
         company:"",
-        key:20
     },
     {
         name: "Travis Nelson",
         position: "Democratic National Committee Member",
         company:"",
-        key:21
+    },
+    {
+        name: "Sharon Gary Smith",
+        position: "NAACP",
+        company:"",
+    },
+    {
+        name: "Miguel Moreno",
+        position: "Willamette Chicano Alliance",
+        company:"",
+    },
+    {
+        name: "Maria Rubio",
+        position: "Centro Cultural",
+        company:"",
+    },
+    {
+        name: "Dr. Katie Riley",
+        position: "Washington County Kids President",
+        company:"",
     },
     {
         name: "Jaime Rodriguez",
         position: "American Federation of Teachers - Oregon President",
         company:"",
-        key:22
     },
     {
         name: "Danielle Garza",
         position: "LiUNA Local 737 Political and Legislative Assistant",
         company:"",
-        key:23
     }
 ]

4446f3008d31a96478bcb09266cb40c2907f14a3
bmediax <contact@bmediax.com>
2021-04-06 16:52:29 -0700

    Added correct alt

--- src/components/sections/PrioritiesSection.js
+++ src/components/sections/PrioritiesSection.js
@@ -11,21 +11,21 @@ const PrioritiesSection = () => {
                 <div className="three-column-option">
                     <div>
                         <center>
-                            <StaticImage src="../../images/gfx/icons/circlePeople.png" alt="This is an imgae" width={120} />
+                            <StaticImage src="../../images/gfx/icons/circlePeople.png" alt="Inclusive Icon" width={120} />
                         </center>
                         <h2> Inclusive, equitable education for all students.</h2>
                         {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, sequi</p> */}
                     </div>
                     <div>
                         <center>
-                            <StaticImage src="../../images/gfx/icons/heartHand.png" alt="This is an image" width={120} />
+                            <StaticImage src="../../images/gfx/icons/heartHand.png" alt="Heart hand Icon" width={120} />
                         </center>
                         <h2>Stronger partnerships with K-12 school districts.</h2>
                         {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, sequi</p> */}
                     </div>
                     <div>
                         <center>
-                            <StaticImage src="../../images/gfx/icons/eye.png" alt="This is an imgae" width={120} />
+                            <StaticImage src="../../images/gfx/icons/eye.png" alt="Transparent Eye Icon" width={120} />
                         </center>
                         <h2>Transparent commitment within the PCC Board</h2>
                         {/* <p>To nurture a collaborative culture of Diversity, Equity, and Inclusion among faculty, staff, and administrators throughout Portland Community College and beyond. </p> */}
